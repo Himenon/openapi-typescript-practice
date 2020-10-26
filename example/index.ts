b3476528d893340d0e6460919de78fe71814ec66
Himenon <k.himeno314@gmail.com>
2020-10-27 00:52:32 +0900

    chore: add example

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,18 @@
-import { ArticleApi } from "../lib/Article"
+const { ArticleApi, Configuration } = require("../lib/Article");
+// import { ArticleApi, Configuration } from "../lib/Article";
 
 const main = async () => {
-  const client = new ArticleApi();
+  const client = new ArticleApi(
+    new Configuration({
+      fetchApi: require("node-fetch"),
+    })
+  );
 
   const result = await client.get({
-    articleId: 123
+    articleId: 123,
   });
 
-  result.body;
-}
+  console.log(result);
+};
 
 main().catch(console.error);
